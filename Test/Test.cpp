04b2365eced468f5ec4959f5ef938e6a8b28f181
David Busch <busch@users.noreply.github.com>
2020-09-08 13:50:32 -0500

    Example

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -8,17 +8,22 @@
 
 using namespace std;
 
+// Outputs first name to standard output device.
 void printFirstName();
+
+
 void printLastName();
 
-string firstName = "Joe";
+string firstName = "Joe";		// First name of our customer.
 string lastName = "Doe";
 
 int main()
 {
+	// Demo of using identifiers for output
 	cout << firstName << firstName << firstName << firstName << firstName;
 
 
+	// Outputting customer first and last name.
 	cout << "Name: ";
 	printFirstName();
 	cout << " ";
